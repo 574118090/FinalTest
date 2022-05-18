0c888770c212c7c81d7860c69f576bbb5ba3a511
1Ayaka <105589073+1Ayaka@users.noreply.github.com>
2022-05-18 16:43:06 +0800

    123

--- FinalTest/main.cpp
+++ FinalTest/main.cpp
@@ -2,11 +2,16 @@
 #include"point.h"
 #include"vec.h"
 #include"caculate_output.h"
+#include"line-segment.h"
 signed main() {
 	vec testPoint(1, 2);
 	vec testPoint2(3, 4);
 	vec tesP = testPoint - testPoint2;
 	point outp(1,1);
 	cout << Caculate::Cross(1, 1, 1, 1);
+
+	//line a(1,1,2,2);
+	//a.Output();
+
 	return 0;
 }
